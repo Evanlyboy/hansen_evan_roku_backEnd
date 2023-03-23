28acf6f4a0d838a1b13e953d4498a3d04d164058
unknown <evanhansen20@yahoo.ca>
2023-03-23 12:53:20 -0400

    finished example

--- routes/api.js
+++ routes/api.js
@@ -15,6 +15,57 @@ router.get('/', (req, res) => {
     res.json({message: 'hit ums API root'});
 })
 
+// try to authenticate a user via the login route
+router.post("/login", (req, res) => {
+  console.log('hit the login route');
+  console.log('user data:', req.body);
+
+  pool.getConnection(function(err, connection) {
+    if (err) throw err; // not connected!
+   
+    // Use the connection
+    connection.query(`SELECT username, password FROM users WHERE username="${req.body.username}"`, function (error, results) {
+      // When done with the connection, release it.
+      connection.release();
+   
+      // Handle error after the release.
+      if (error) throw error;
+
+      let result = { message: '' };
+
+      // what if the user doesn't exist? no username
+      if (results.length == 0) {
+        result.message = 'no user';
+      } else if (results[0].password !== req.body.password){
+        // no matching password. Go directly to jail. Do not pass GO, do not collect $200
+        result.message = 'wrong password'
+      } else {
+        // all error checks passed
+        result.message = 'success',
+
+        delete results[0].password;
+        result.user = results[0];
+      }
+
+      // results.forEach(user => {
+      //   // sanitize the data
+      //   delete user.password;
+      //   delete user.fname;
+      //   delete user.lname;
+
+      //   // if no avatar, set a default
+      //   if(!user.avatar) {
+      //     user.avatar = "temp_avatar.jpg";
+      //   }
+      // })
+   
+      // Don't use the connection here, it has been returned to the pool.
+      res.json(result);
+    });
+  });
+})
+
+
 // Retrieve all users from a database
 router.get('/users', (req, res) => {
 
@@ -30,11 +81,23 @@ router.get('/users', (req, res) => {
        
           // Handle error after the release.
           if (error) throw error;
+
+          results.forEach(user => {
+            // sanitize the data
+            delete user.password;
+            delete user.fname;
+            delete user.lname;
+
+            // if no avatar, set a default
+            if(!user.avatar) {
+              user.avatar = "temp_avatar.jpg";
+            }
+          })
        
           // Don't use the connection here, it has been returned to the pool.
           res.json(results);
         });
-      });
+    });
     // -----------------------------------------------------------------------------------------
 })
 
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const server = express();
-const port = process.env.port || 5000;
+const port = process.env.port || 5050;
+
+// configure the back end to accept incoming data
+// either as a JSON payload or as form data (encoded url strings)
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: false })); //url?key=value&&key=value
 
 // User Management System Route. For managing user data and such
 server.use('/ums', require('./routes/api'));
